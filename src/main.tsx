7e364cbdf99fc6bef6695bde5b920dbcb5809907
saltandsovereigntypod <saltandsovereigntybusiness@gmail.com>
2026-08-06 15:14:53 -0500

    Enable Prythian rank-based reveal gates

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './prythian-experience-bootstrap';
+import './prythian-reveal-gates';
 import CoreFullApp from './CoreFullApp';
 import './creative-libraries.css';
 import './stage-controls.css';
